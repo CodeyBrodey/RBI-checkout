6e4ed7f3a6d4eda433d8827d3e2c686cdca59587
CodeyBrodey <102107941+CodeyBrodey@users.noreply.github.com>
2024-03-20 22:58:04 -0300

    Checkout start

--- RBI-checkout/src/App.jsx
+++ RBI-checkout/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header/Header'
+import Checkout from './components/Checkout/Checkout'
 
 const App = () => {
 
   return (
     <>
       <Header />
+      <Checkout />
     </>
   )
 }
---
+++ RBI-checkout/src/components/Checkout/Checkout.jsx
@@ -0,0 +1,22 @@
+import './Checkout.css'
+
+const Checkout = () => {
+
+    return(
+        <section>
+            <div>
+                <h1 className='page-title'>Checkout</h1>
+            </div>
+
+            <div>
+
+            </div>
+
+            <div>
+
+            </div>
+        </section>
+    )
+}
+
+export default Checkout
